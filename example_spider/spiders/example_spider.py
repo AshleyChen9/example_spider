b37b79da6f891b3d5eaedf50f3b92b62899b777b
Zhang, Fan <fanz@aidigger.com>
2017-04-18 11:23:02 +0800

    items, urls, delays

--- example_spider/spiders/example_spider.py
+++ example_spider/spiders/example_spider.py
@@ -1,16 +1,26 @@
-import scrapy
+from scrapy.spiders import Spider
+from scrapy import Field, Item, Request
 
-class ExampleSpider(scrapy.Spider):
-    name = "example"
+class QuotesSpiderItem(Item):
+    text = Field()
+    author = Field()
+    tags = Field()
+
+class QuotesSpider(Spider):
+    name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
     ]
 
     def parse(self, response):
         for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
+            item = QuotesSpiderItem()
+            item['text'] = quote.css('span.text::text').extract_first()
+            item['author'] = quote.css('small.author::text').extract_first()
+            item['tags'] = quote.css('div.tags a.tag::text').extract()
+            yield item
 
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield Request(next_page, callback=self.parse)
